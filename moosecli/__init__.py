906a686ecc6be459eca727c86f16f6a05338e8bd
b-long <(none)>
2022-12-04 09:52:10 -0500

    First pass at fully-native binaries

--- moosecli/__init__.py
+++ moosecli/__init__.py
@@ -1,8 +1,15 @@
 # pylint: disable=missing-module-docstring
 __version__ = "0.1.0"
 import typer
+import sys
 
 from moosecli.lib import main
 
 if __name__ == "__main__":
     typer.run(main)
+
+
+from ._package import setup_frozen_package
+
+if getattr(sys, "frozen", False):
+    setup_frozen_package()
