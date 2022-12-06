95678365565d9e826677ff12c57c19e51d771261
b-long <b-long@users.noreply.github.com>
2022-12-06 09:13:46 -0500

    Format code & fix imports

--- moosecli/__init__.py
+++ moosecli/__init__.py
@@ -1,15 +1,15 @@
 # pylint: disable=missing-module-docstring
 __version__ = "0.1.0"
-import typer
 import sys
+
+import typer
 
 from moosecli.lib import main
 
+from ._package import setup_frozen_package
+
 if __name__ == "__main__":
     typer.run(main)
-
-
-from ._package import setup_frozen_package
 
 if getattr(sys, "frozen", False):
     setup_frozen_package()
